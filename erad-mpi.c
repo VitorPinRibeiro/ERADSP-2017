469a8e1520f1e33abb360b2a4d17e7da0b3bd9f1
vitorpinribeiro <GRADeradsp@ltsp826.(none)>
2017-04-08 11:15:53 -0300

    msg

--- erad-mpi.c
+++ erad-mpi.c
@@ -6,11 +6,13 @@ int main (int argc, char* argv[]){
 	
 	int me, qt;
 	
-	MPI_Init();
+	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &me);
 	MPI_Comm_size(MPI_COMM_WORLD, &qt);
 	
 	printf("Hello from process %d out of %d\n", me, qt);
+	
+	MPI_Finalize();
 	
 	return EXIT_SUCCESS;
 }
